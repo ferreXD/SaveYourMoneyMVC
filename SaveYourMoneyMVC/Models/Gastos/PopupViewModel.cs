8ad85a6bb486c6fa9cde4a4537bf48af85971a36
ferreXD <39083343+ferreXD@users.noreply.github.com>
2022-10-29 12:49:39 +0200

    aaaa

--- SaveYourMoneyMVC/Models/Gastos/PopupViewModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace SaveYourMoneyMVC.Models.Gastos
-{
-    public class PopupViewModel
-    {
-        public string? Type { get; set; }
-        public string? PopupMessage { get; set; }
-    }
-}
